1f27760226aa99cc20a033b235304b181b2abcf5
Mercy <contact@larkin.space>
2017-08-28 21:10:05 +0100

    Shorter logging

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 const log = require('../functions/logger')
 
 module.exports = (bot) => {
-  log(`Logged in as ${bot.user.tag} and ready in ${bot.guilds.size} servers with ${bot.channels.size} visible channels.`);
+  log(`Logged in as ${bot.user.tag} in ${bot.guilds.size} servers with ${bot.channels.size} visible channels.`);
 }
