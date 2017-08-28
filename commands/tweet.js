0dc0e2cc356f521e4e9e8c28883ed8b6373e9ea8
Mercy <contact@larkin.space>
2017-08-28 18:06:03 +0100

    Added tweet command

---
+++ commands/tweet.js
@@ -0,0 +1,29 @@
+const config = require('../config.json');
+
+const twit = require('twit');
+const twitRunner = new twit({
+  consumer_key: config.twitter.consumer_key,
+  consumer_secret: config.twitter.consumer_secret,
+  access_token: config.twitter.access_token,
+  access_token_secret: config.twitter.access_token_secret
+});
+
+module.exports = {
+  commands: [
+    'tweet'
+  ],
+  usage: 'tweet <text to tweet>',
+  description: 'Tweet messages from Discord!',
+  category: 'Information',
+  execute: (bot, msg, args) => {
+    if(!args) return msg.edit('**Missing text to tweet.**');
+    if(args.join(' ').length > 140) return msg.edit('**Tweet too long.**');
+
+    twitRunner.post('statuses/update', { status: args.join(' ') }, function(err, data, response) {
+      if(err) return console.error(err);
+      // console.log(data);
+      // console.log(response);
+      msg.edit('**Tweet sent!**\nhttps://twitter.com/MercyDev_/status/' + data.id_str)
+    })
+  }
+};
