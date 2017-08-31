92f8661c32b801d406e6bc65292faea1f5c8038e
Mercy <jensen@larkin.space>
2017-08-31 19:20:55 +0100

    Screw it, I'll console.log it

--- functions/logger.js
+++ functions/logger.js
@@ -1,5 +1,5 @@
 const dateformat = require('dateformat');
 
 module.exports = (msg) => {
-  process.stdout.write(dateformat(Date.now(), 'hh:MM:ss TT') + ' | [Mercy-Selfbot] ' + msg + '/n'); // Fucking ESLINT doesn't want `console.log()` so eat my process ya dick
+  console.log(dateformat(Date.now(), 'hh:MM:ss TT') + ' | [Mercy-Selfbot] ' + msg);
 };
