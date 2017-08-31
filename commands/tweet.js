7c744e443272e1113a715f1f9c7075065aebc297
Mercy <jensen@larkin.space>
2017-08-31 17:12:34 +0100

    More Codacy

--- commands/tweet.js
+++ commands/tweet.js
@@ -1,7 +1,7 @@
 const config = require('../config.json');
 
-const twit = require('twit');
-const twitRunner = new twit({
+const Twit = require('twit');
+const twitRunner = new Twit({
   consumer_key: config.twitter.consumer_key,
   consumer_secret: config.twitter.consumer_secret,
   access_token: config.twitter.access_token,
@@ -16,11 +16,18 @@ module.exports = {
   description: 'Tweet messages from Discord!',
   category: 'Fun',
   execute: (bot, msg, args) => {
-    if(args.length < 1) return msg.edit('**Missing text to tweet.**');
-    if(args.join(' ').length > 140) return msg.edit('**Tweet too long.**');
+    if(args.length < 1) {
+      return msg.edit('**Missing text to tweet.**');
+    }
+
+    if(args.join(' ').length > 140) {
+      return msg.edit('**Tweet too long.**');
+    }
 
     twitRunner.post('statuses/update', { status: args.join(' ') }, function(err, data, response) {
-      if(err) return console.error(err);
+      if(err) {
+        return;
+      }
       // console.log(data);
       // console.log(response);
       msg.edit('**Tweet sent!**\nhttps://twitter.com/' + data.user.screen_name + '/status/' + data.id_str)
