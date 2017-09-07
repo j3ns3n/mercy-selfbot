3f37bb631b1af54e67f4a70f54add62376d672a0
Mercy <jensen@larkin.space>
2017-09-07 19:06:34 +0100

    Might fix eval

--- commands/eval.js
+++ commands/eval.js
@@ -9,17 +9,15 @@ module.exports = {
   category: 'Utility',
   execute: (bot, msg, args) => {
     if (args.length > 0) {
-      try {
         let result = Promise.resolve(eval(args.join(' ')));
 
         result.then((result) => {
           cleanAndPost(result, msg, args, bot);
+        }).catch((error) => {
+          msg.edit('**Input:**\n```js\n' + args.join(' ') + '```\n**An error occured when attempting to evaluate code!**\n```js\n' + error + '```').catch((error) => {
+            throw new Error(error);
+          });
         });
-      } catch (error) {
-        msg.edit('**Input:**\n```js\n' + args.join(' ') + '```\n**An error occured when attempting to evaluate code!**\n```js\n' + error + '```').catch((error) => {
-          throw new Error(error);
-        });
-      }
     } else {
       msg.edit('**Missing code to evaluate.**');
     }
