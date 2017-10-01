c90ca800dc1ed5942a82dcda1bc036a2e13f0e82
lepon01 <leondrolio@gmail.com>
2017-10-02 00:08:13 +0100

    Move footer to external file, remove unneeded spotify, used less callbacks

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,30 +1,6 @@
 const snekfetch = require('snekfetch');
 const { RichEmbed } = require('discord.js');
 
-const getBotInfo = (id, msg) => {
-  snekfetch.get('https://discordbots.org/api/bots/' + id).then((r) => {
-    const embed = new RichEmbed()
-    .setColor(0x5B8DEA)
-    .setTimestamp()
-    .setTitle('Bot Info')
-    .setFooter('https://github.com/LarK1n/mercy-selfbot')
-    .setThumbnail('https://cdn.discordapp.com/avatars/' + id + '/a_' + r.body.avatar + '.webp')
-    .addField('ID', id)
-    .addField('Tag', r.body.username + '#' + r.body.discriminator, true)
-    .addField('Short Description', r.body.shortdesc)
-    .addField('Library', r.body.lib, true)
-    .addField('Prefix', r.body.prefix)
-    .addField('Upvotes', r.body.points, true)
-    .addField('Server Count', r.body.server_count || 'This bot doesn\'t post server count!', true)
-    .addField('Owner(s)', '<@' + r.body.owners.join('>\n<@') + '>')
-    .addField('Invite', '[Here](https://discordapp.com/oauth2/authorize?scope=bot&permissions=0&client_id=' + id + ')', true);
-    msg.edit('', { embed });
-  })
-  .catch(() => {
-    return msg.edit('**The bot you are looking for was not found!**');
-  });
-};
-
 module.exports = {
   commands: [
     'botinfo'
@@ -36,10 +12,35 @@ module.exports = {
     if(args.length < 1) {
       return msg.edit('**Please provide a bot mention or ID**');
     }
+
+    let id = 0;
+
     if(msg.mentions.users.first()) {
-      getBotInfo(msg.mentions.users.first().id, msg);
+      id = msg.mentions.users.first().id;
     } else {
-      getBotInfo(args[0], msg);
+      id = args[0];
     }
+
+    snekfetch.get('https://discordbots.org/api/bots/' + id).then((res) => {
+      const embed = new RichEmbed()
+      .setColor(0x5B8DEA)
+      .setTimestamp()
+      .setTitle('Bot Info')
+      .setFooter(bot.config.strings.github)
+      .setThumbnail(`https://cdn.discordapp.com/avatars/${id}/a_${res.body.avatar}.webp`)
+      .addField('ID', id)
+      .addField('Tag', `${res.body.username}#${res.body.discriminator}`, true)
+      .addField('Short Description', res.body.shortdesc)
+      .addField('Library', res.body.lib, true)
+      .addField('Prefix', res.body.prefix)
+      .addField('Upvotes', res.body.points, true)
+      .addField('Server Count', typeof res.body.server_count === 'number' ? res.body.server_count : 'This bot doesn\'t post server count!', true)
+      .addField('Owner(s)', '<@' + res.body.owners.join('>\n<@') + '>')
+      .addField('Invite', '[Here](https://discordapp.com/oauth2/authorize?scope=bot&permissions=0&client_id=' + id + ')', true);
+      msg.edit('', { embed });
+    })
+    .catch(() => {
+      return msg.edit('**The bot you are looking for was not found!**');
+    });
   }
 };
