c97bdcb961689c224202322562e403324c737bd8
Mercy <jensen@larkin.space>
2017-10-02 00:34:35 +0100

    Damnit

--- commands/embedsay.js
+++ commands/embedsay.js
@@ -10,11 +10,12 @@ module.exports = {
   description: 'Takes words you put in and Puts them in a embed!',
   category: 'Other',
   execute: (bot, msg, args) => {
-  if (args.length > 0) {
-    const embed = new RichEmbed()
-    .setTitle(args.join(' '));
-    msg.edit({embed});
-  } else {
-    return msg.edit('**Add something after the command silly!**');
+    if (args.length > 0) {
+      const embed = new RichEmbed()
+      .setTitle(args.join(' '));
+      msg.edit({embed});
+    } else {
+      return msg.edit('**Add something after the command silly!**');
+    }
   }
 }
