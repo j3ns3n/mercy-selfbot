8b5b83679bef613ecc517b4975fae2214a2e92b1
Mdstumpfy123 <tgb.2gamingbros@gmail.com>
2017-10-01 18:21:49 -0500

    Added 1 command (#8)
    * Create embedsay.js
    
    * Update embedsay.js
    
    * Update embedsay.js
    
    * Standardising

---
+++ commands/embedsay.js
@@ -0,0 +1,18 @@
+const { RichEmbed } = require('discord.js');
+
+module.exports = {
+  commands: [
+    'embedsay',
+    'say',
+    'esay'
+  ],
+  usage: 'embedsay <texthere>',
+  description: 'Takes words you put in and Puts them in a embed!',
+  category: 'Other',
+  execute: (bot, msg, args) => {
+  if (args.length > 0) msg.edit('**Add something after the command silly!**') return;
+    const embed = new RichEmbed()
+    .setTitle(args.join(' '));
+    msg.edit({embed})
+  }
+};
