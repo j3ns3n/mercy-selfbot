a94ed71c0f2bd47c93fa448b1e2ef312c4b89aaf
Mercy <jensen@larkin.space>
2017-10-02 00:28:03 +0100

    I just took a good look at the code and wtf did you commit?

--- commands/embedsay.js
+++ commands/embedsay.js
@@ -10,9 +10,11 @@ module.exports = {
   description: 'Takes words you put in and Puts them in a embed!',
   category: 'Other',
   execute: (bot, msg, args) => {
-  if (args.length > 0) msg.edit('**Add something after the command silly!**') return;
+  if (args.length > 0) {
     const embed = new RichEmbed()
     .setTitle(args.join(' '));
     msg.edit({embed})
+  } else {
+    return msg.edit('**Add something after the command silly!**');
   }
 };
