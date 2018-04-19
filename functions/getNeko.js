1692041109014525a6729b58d16b967a16a84b4a
Mercy <jensen@larkin.space>
2018-04-19 19:54:22 +0100

    Revert "Fixed error"
    This reverts commit f54ca1549504092e7cb6e68238650850ff263e57.

--- functions/getNeko.js
+++ functions/getNeko.js
@@ -1,9 +1,9 @@
-const snekfetch = require("snekfetch");
+const snekfetch = require('snekfetch');
 
-const getURL = endpoint => {
-  snekfetch.get(`https://nekos.life/api/${endpoint}`).then(res => {
+const getURL = (endpoint) => {
+  snekfetch.get(`https://nekos.life/api/${endpoint}`).then((res) => {
     if (res.status !== 200) {
-      return msg.channel.send("**An error has occurred!**");
+      return msg.channel.send('**An error has occurred!**');
     }
     return res.body.url;
   }
